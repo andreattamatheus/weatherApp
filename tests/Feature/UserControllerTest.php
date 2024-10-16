561a0d194c3132b03e5107231d36a01517f4c68b
Matheus Andreatta <matheus.andreatta@pdmfc.com>
2024-10-16 18:57:24 -0300

    Refactor UserControllerTest and WeatherApiServiceTest Refactor access modifiers in CreateLocationForecast.php Refactor validation error response in AuthLoginRequest and LocationStoreRequest Refactor UserController and LocationForecastController Refactor error logging in LocationForecastController and UserController

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -11,15 +11,18 @@ use Illuminate\Support\Facades\Queue;
 
 class UserControllerTest extends TestCase
 {
+    protected $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->make();
+    }
+
     public function test_store_location_success()
     {
-        // Fake the queue for the CreateLocationForecast job
         Queue::fake();
 
-        // Get a user
-        $user = User::factory()->make();
-
-        // Prepare the request data
         $requestData = [
             'city' => 'Test City',
             'state' => 'Test State',
@@ -29,31 +32,24 @@ class UserControllerTest extends TestCase
             'condition' => 'Sunny',
         ];
 
-        // Send the POST request
-        $response = $this->actingAs($user, 'sanctum')->postJson('/api/v1/users/locations', $requestData);
+        $response = $this->actingAs($this->user, 'sanctum')->postJson('/api/v1/users/locations', $requestData);
 
-        // Assert the response
         $response->assertStatus(200)
             ->assertJson([
                 'message' => 'Location saved successfully!',
             ]);
 
         // Check that the job was dispatched with correct data
-        Queue::assertPushed(CreateLocationForecast::class, function ($job) use ($requestData, $user) {
+        Queue::assertPushed(CreateLocationForecast::class, function ($job) use ($requestData) {
             $weatherData = ForecastResource::make($requestData)->resolve();
-            return $job->weatherData === $weatherData && $job->user->is($user);
+            return $job->weatherData === $weatherData && $job->user->is($this->user);
         });
     }
 
     public function test_store_location_failure()
     {
-        // Fake the queue and the logger
         Queue::fake();
 
-        // Get a user
-        $user = User::factory()->make();
-
-        // Send the POST request with invalid data (e.g., missing 'city')
         $requestData = [
             'state' => 'Test State',
             'date' => now()->toDateString(),
@@ -62,9 +58,8 @@ class UserControllerTest extends TestCase
             'condition' => 'Sunny',
         ];
 
-        $response = $this->actingAs($user, 'sanctum')->postJson('/api/v1/users/locations', $requestData);
+        $response = $this->actingAs($this->user, 'sanctum')->postJson('/api/v1/users/locations', $requestData);
 
-        // Assert the failure response
         $response->assertStatus(422)
             ->assertJsonValidationErrors([
                 'city'
